07dfe15f38e0b86426d79e594b7b421ae22342eb
Zhurbey Artem <zhurbeyartem@gmail.com>
2023-11-18 14:38:14 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -7,15 +7,22 @@ const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const btn = document.querySelector('.load-more');
 
-let lightbox = null;
+let lightbox = new SimpleLightbox('.gallery__link', {
+  caption: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
 const API_KEY = '40728025-816c059da6666ef8bc43bfdc5';
 let page = 1;
-
 
 const res = async e => {
   e.preventDefault();
   try {
-    const inp = document.querySelector('.inp').value;
+    gallery.innerHTML = '';
+
+    if (e.target.type === 'button') page++;
+    else page = 1;
+    const inp = form.elements[0].value;
 
     const { data } = await axios.get('https://pixabay.com/api/', {
       params: {
@@ -32,8 +39,29 @@ const res = async e => {
       return Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-    const arr = data.hits.map(
-      el => `
+
+    const arr = addElements(data.hits);
+    gallery.insertAdjacentHTML('beforeend', arr.join(''));
+    lightbox.refresh();
+    if (page < Math.ceil(data.totalHits / 20))
+      btn.classList.toggle('hidden', false);
+    else btn.classList.toggle('hidden', true);
+
+    Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+const loadMore = e => {
+  gallery.innerHTML = '';
+  lightbox.refresh();
+  res(e);
+};
+
+const addElements = arr => {
+  return arr.map(
+    el => `
       <div class="photo-card">
         <a class="gallery__link" href="${el.largeImageURL}">
           <img src="${el.webformatURL}" alt="${el.tags}" class="photo-img" loading="lazy" />
@@ -58,26 +86,9 @@ const res = async e => {
         </div>
       </div>
     `
-    );
-    gallery.insertAdjacentHTML('beforeend', arr.join(''));
-    lightbox =  new SimpleLightbox('.gallery__link', {
-   caption: true,
-   captionDelay: 250,
-   captionsData: 'alt',
- });
-    Notify.success(`Hooray! We found ${data.totalHits} images.`);
-  } catch (error) {
-    console.error('Error:', error);
-  }
+  );
 };
 
-const loadMore = (e) => {
-  gallery.innerHTML = '';
-  lightbox.refresh();
-  page++;
-  res(e)
-}
-
-btn.addEventListener('click', loadMore)
+btn.addEventListener('click', loadMore);
 
 form.addEventListener('submit', e => res(e));
